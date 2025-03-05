285710eba19c8186e9d0c87b2b8fec61721dfeeb
Jesus Sierra <sierraja06@gmail.com>
2025-03-05 00:09:39 -0500

    Arreglando los cambios

--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductoController } from './producto.controller';
 import { ProductoService } from './producto.service';
+import { Producto, ProductoSchema } from './producto.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Producto.name, schema: ProductoSchema }])
+  ],
   controllers: [ProductoController],
-  providers: [ProductoService]
+  providers: [ProductoService],
+  exports: [ProductoService], // Exportarlo si lo usas en otros módulos
 })
-export class ProductoModule {}
+export class ProductoModule { }
