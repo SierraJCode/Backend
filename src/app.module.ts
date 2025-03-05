0860996c61d7e1ce0b25715a87c391add3eeda84
Jesus Sierra <sierraja06@gmail.com>
2025-03-04 22:07:36 -0500

    BackEnd Funcionando

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [UsuariosModule],
+  imports: [UsuariosModule, MongooseModule.forRoot('mongodb://localhost:27017/usuariosDB')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Usuario, UsuarioSchema } from './usuario.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Usuario.name, schema: UsuarioSchema }])],
   providers: [UsuariosService],
   controllers: [UsuariosController]
 })
-export class UsuariosModule {}
+export class UsuariosModule { }
